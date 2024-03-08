8a86fdd701bae1b681d09589e778335284227c5f
gateremark <mark5gatere@gmail.com>
2024-03-08 18:59:01 +0300

    refactor: add submitting details notice

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,10 @@ import supabase from "@/lib/SupabaseClient";
 import { UploadData } from "@/app/api/supabase/route";
 import axios from "axios";
 
-
 export default function Home() {
     const [uploading, setUploading] = useState(false);
     const [fileUrl, setFileUrl] = useState<string | null>(null);
-   
+    const [sendDetails, setSendDetails] = useState(false);
     const [formData, setFormData] = useState({
         goal1: "",
         phone: "",
@@ -34,7 +33,7 @@ export default function Home() {
             .data.publicUrl;
         console.log("FileUrl: ", fileUrl);
         setFileUrl(fileUrl);
-        
+
         if (error) {
             throw error;
         }
@@ -47,13 +46,12 @@ export default function Home() {
         },
         maxFiles: 1,
         onDrop: async (acceptedFiles) => {
-            
             const file = acceptedFiles[0];
-            
+
             try {
                 setUploading(true);
                 const data = await uploadFile(file);
-                
+
                 toast.success("File uploaded successfully!");
             } catch (error) {
                 console.log(error);
@@ -79,6 +77,8 @@ export default function Home() {
 
         const response = await axios.post("/api/supabase", projectData);
         try {
+            setSendDetails(true);
+
             const response2 = await fetch("/api/user", {
                 method: "POST",
                 headers: {
@@ -92,11 +92,12 @@ export default function Home() {
             console.error("Error:", error);
         } finally {
             console.log("Finished processing request");
+            setSendDetails(false);
         }
 
         console.log("Response: ", response);
         if (response.status === 200) {
-            toast.success("Form submitted successfully!");
+            toast.success("Details submitted successfully!");
             setFormData({
                 goal1: "",
                 phone: "",
@@ -183,7 +184,7 @@ export default function Home() {
                     )}
                 </div>
 
-                {fileUrl ? (
+                {fileUrl && !sendDetails ? (
                     <button type="submit" onClick={onSubmit} className=" w-fit">
                         <p className=" text-2xl bg-gradient-to-r dark:from-[#00c6ff] dark:to-[#0072ff] from-[#0072ff] to-[#00c6ff]  bg-clip-text text-transparent font-semibold">
                             Submit
@@ -200,6 +201,14 @@ export default function Home() {
                             Submit
                         </p>
                     </button>
+                )}
+                {sendDetails && (
+                    <div className="flex justify-center items-center flex-col gap-1">
+                        <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+                        <p className="text-gray-500 text-sm mt-2">
+                            Submitting Details...
+                        </p>
+                    </div>
                 )}
             </div>
             {/* <div>
